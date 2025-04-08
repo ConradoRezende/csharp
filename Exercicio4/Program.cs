9a45585ed51b09aa3f2d8deb02f837db2c1078e0
ConradoRezende <conrado.neto27@hotmail.com>
2025-04-08 20:58:05 -0300

    Add files via upload

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization; //importa a cultura variável
+
+namespace SISTEMADEALUNO
+
+//4. Exercício: Maior Número 
+//Solicite dois números ao usuário e exiba na tela qual deles é o maior.
+//Utilize estruturas condicionais como if para fazer a verificação
+{
+    class Exercicio4
+    {
+    static void Main(string[] args) //método que executa o programa
+    {
+        Console.WriteLine("Digite 2 números: "); 
+        double num1 = Convert.ToDouble((Console.ReadLine()));
+        double num2 = Convert.ToDouble((Console.ReadLine()));
+            
+        if (num1 > num2) {
+        Console.WriteLine("O maior número é: " + num1 + ".");          
+        } else if (num2 > num1) {
+            Console.WriteLine("O maior número é: "+ num2 + ".");
+        } else 
+            Console.WriteLine("Os números são iguais.");
+        }
+    }
+
+}
---
+++ Exercicio5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization; //importa a cultura variável
+
+namespace SISTEMADEALUNO
+
+//5. Exercício: Verificar Par ou Ímpar
+//Peça ao usuário que digite um número e informe se ele é par ou ímpar.
+//Dica: Utilize o operador % (módulo) para verificar se o resto da divisão por 2 é igual a 0.
+
+{
+    class Exercicio5
+    {
+    static void Main(string[] args) //método que executa o programa
+    {
+        Console.WriteLine("Digite um número: "); 
+        double num = Convert.ToDouble((Console.ReadLine()));
+                    
+        if (num % 2 == 0) {
+        Console.WriteLine("O número " + num + " é par.");          
+        } else {
+            Console.WriteLine("O número é impar.");
+        }
+    }
+    }
+}
